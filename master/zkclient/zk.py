0acaaabbc43ac20593191fb56c54ca9204800847
JaqEst <jade2ggle@gmail.com>
2024-05-06 18:48:38 +0800

    encapsule zkclient into a singleton

---
+++ master/zkclient/zk.py
@@ -0,0 +1,19 @@
+from kazoo.client import KazooClient
+
+
+class ZkClient:
+    # singleton
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(ZkClient, cls).__new__(cls, args, kwargs)
+        return cls._instance
+
+    def __init__(self, hosts):
+        self.zk = KazooClient(hosts=hosts)
+
+    @staticmethod
+    def get():
+        assert(ZkClient._instance is not None)
+        return ZkClient._instance
