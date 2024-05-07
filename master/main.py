8474da728a6f1a88674181ea8e9054d48a10d233
JaqEst <jade2ggle@gmail.com>
2024-05-07 22:07:53 +0800

    run master with zkclient connected

---
+++ master/main.py
@@ -0,0 +1,5 @@
+from master import Master
+
+if __name__ == '__main__':
+    master = Master('dev')
+    master.start()
---
+++ master/master.py
@@ -0,0 +1,35 @@
+import signal
+
+from config import *
+from zkclient import ZkClient
+from rscluster import RsCluster
+
+
+class Master:
+    def __init__(self, ctype):
+        self._zk = ZkClient(ZK_ADDR)
+        self._cluster = RsCluster(self._zk)
+
+        signal.signal(signal.SIGINT, self.stop)
+
+    def start(self):
+        self._zk.start()
+        # create Znode '/master'
+        # TODO: write snapshot
+        ok = self._zk.create('/master', ephemeral=True)
+        if ok is False:
+            self.stop()     # exit when master already exists
+
+        # read regions
+        self._cluster.init()
+
+        print('Master READY')
+        # Thrift-based server
+        while True:
+            pass
+
+    def stop(self, signum=None, frame=None):
+        self._zk.stop()
+
+        print('Master EXIT')
+        exit(0)
