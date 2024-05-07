4102a01c22e18181fc928bc1509f72c736d0836d
JaqEst <jade2ggle@gmail.com>
2024-05-07 22:09:03 +0800

    init master inner data structure

---
+++ master/watcher/__init__.py
@@ -0,0 +1,1 @@
+from .rswt import RsWatcher
---
+++ master/watcher/rswt.py
@@ -0,0 +1,18 @@
+from .basewt import BaseWatcher
+
+
+class RsWatcher(BaseWatcher):
+    def __init__(self, cluster):
+        self._cluster = cluster
+        self.ignore = True  # simply ignore the very first time call
+
+    def __call__(self, children):
+        if self.ignore is True:
+            print('RsWatcher REGISTERED')
+            self.ignore = False
+            return
+
+        print('RsWatcher CALLED')
+        with self._cluster.sem:
+            # TODO: adjust our regions as well as tables
+            pass
