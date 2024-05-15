d8a8c215224d56e2da44ff69962dd9e13b4f1b51
HSX <569075247@qq.com>
2024-05-15 21:51:04 +0800

    add master service

--- region/src/main/java/main/Region.java
+++ region/src/main/java/main/Region.java
@@ -1,11 +1,22 @@
 package main;
 
+import api.r2r;
 import database.DBConnection;
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TMultiplexedProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
 import task.ClientTask;
+import task.MasterCreateTask;
 import task.MasterTask;
 import task.RegionTask;
+import utils.MetaTable;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.concurrent.ArrayBlockingQueue;
 
 public class Region
@@ -30,7 +41,7 @@ public class Region
                 }
                 switch (task.type) {
                     case READ:
-                        System.out.println("[READ] " + task);
+                        System.out.println("[CLIENT READ] " + task);
                         try {
                             DBConnection.query(task.sql);
                         } catch (SQLException e) {
@@ -39,7 +50,7 @@ public class Region
                         }
                         break;
                     case WRITE:
-                        System.out.println("[WRITE] " + task);
+                        System.out.println("[CLIENT WRITE] " + task);
                         try {
                             DBConnection.update(task.sql);
                         } catch (SQLException e) {
@@ -50,8 +61,10 @@ public class Region
                 }
             }
         }).start();
-//
+
 //        new Thread(() -> {
+//            HashMap<String, MetaTable> tableHashMap = new HashMap<>();
+//
 //            while (true) {
 //                MasterTask task = null;
 //                try {
@@ -61,40 +74,72 @@ public class Region
 //                }
 //                switch (task.type) {
 //                    case CREATE:
-//                        System.out.println("[CREATE] " + task);
+//                        System.out.println("[Master CREATE] " + task);
+//                        tableHashMap.put(task.table, new MetaTable(true, ((MasterCreateTask) task).regionAddr));
+//                        for (String address: ((MasterCreateTask) task).regionAddr) {
+//                            TTransport transport = null;
+//                            try {
+//                                transport = new TSocket(address.split(":")[0], Integer.parseInt(address.split(":")[1]), 30000);
+//                            } catch (TTransportException e) {
+//                                System.out.println("[Master Error] address " + address + " can not connect!");
+//                                throw new RuntimeException(e);
+//                            }
+//                            TProtocol protocol = new TBinaryProtocol(transport);
+//                            TMultiplexedProtocol server_protocol = new TMultiplexedProtocol(protocol, "R");
+//                            r2r.Client client = new r2r.Client(server_protocol);
+//                            try {
+//                                transport.open();
+//                            } catch (TTransportException e) {
+//                                System.out.println("[Master Error] transport open error!");
+//                                throw new RuntimeException(e);
+//                            }
+//                            try {
+//                                client.sync(((MasterCreateTask) task).sql);
+//                            } catch (TException e) {
+//                                System.out.println(e.getMessage());
+//                                throw new RuntimeException(e);
+//                            }
+//                            transport.close();
+//                        }
+//
 //                        break;
 //                    case DROP:
-//                        System.out.println("[DROP] " + task);
+//                        System.out.println("[Master DROP] " + task);
 //                        break;
 //                    case RECOVER:
-//                        System.out.println("[RECOVER] " + task);
+//                        System.out.println("[Master RECOVER] " + task);
 //                        break;
 //                    case UPGRADE:
-//                        System.out.println("[UPGRADE] " + task);
+//                        System.out.println("[Master UPGRADE] " + task);
 //                        break;
 //                }
 //            }
 //        }).start();
 
-//        new Thread(() -> {
-//            while (true) {
-//                RegionTask task = null;
-//                try {
-//                    task = regionQueue.take();
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }
-//                switch (task.type) {
-//                    case SYNC:
-//
-//                        System.out.println("[SYNC] " + task);
-//                        break;
-//                    case COPY:
-//                        System.out.println("[COPY] " + task);
-//                        break;
-//                }
-//            }
-//        }).start();
+        new Thread(() -> {
+            while (true) {
+                RegionTask task = null;
+                try {
+                    task = regionQueue.take();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                switch (task.type) {
+                    case SYNC:
+                        try {
+                            DBConnection.update(task.exec);
+                        } catch (SQLException e) {
+                            System.out.println(e.getMessage());
+                            throw new RuntimeException(e);
+                        }
+                        System.out.println("[SYNC] " + task);
+                        break;
+                    case COPY:
+                        System.out.println("[COPY] " + task);
+                        break;
+                }
+            }
+        }).start();
 
         System.out.println("region server listening on " + port);
         server.start(port);
